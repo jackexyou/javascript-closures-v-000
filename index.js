75e1a49cbded4455dac12f220add5695401d2bf3
Jack Xu <jackexyou@gmail.com>
2018-04-04 19:51:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 const app = "I don't do much."
 
+<<<<<<< HEAD
+=======
+
+whatsForDinner();
+
+function raspyDoorGuy() {
+  const password = "yarr";
+
+  function givePassword(givenPassword) {
+    if (givenPassword === password) {
+      console.log("Ye may enter");
+    } else {
+      console.log("Begone, landlubber!");
+    }
+  }
+
+  return {
+    givePassword
+  };
+}
+>>>>>>> debd518606da0b0c62296924a230b01a82f84574
